891e39bf044ede778579bfac2275c294e6812295
Федор Арюнов <fedor.arunov@gmail.com>
2018-10-29 22:18:17 +0300

    basic layout + test method done

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
+
+app.listen(port);
+console.log('Listening on port ' + port);
+
+module.exports = app;
+
