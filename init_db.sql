737b82d9dd3e8f030b7e1e52efe5b1bf8175aa84
Арюнов Федор Михайлович <AryunovFM@itc-c.local>
2018-11-02 15:24:16 +0300

    renamed authors to users in init_db

--- init_db.sql
+++ init_db.sql
@@ -1,9 +1,10 @@
 CREATE SEQUENCE public.author_seq;
 
-CREATE TABLE public.authors
+CREATE TABLE public.users
 (
     uid integer NOT NULL DEFAULT nextval('author_seq'::regclass),
-    login character varying(4000) COLLATE pg_catalog."default",    
+    login character varying(4000) COLLATE pg_catalog."default",  
+	registered timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT author_pk PRIMARY KEY (uid)
 );
 
